0143be8c4c7fef3ede869535e8722c103fc5b978
Jesse Rosenberger <git@jro.cc>
2018-04-18 16:15:36 +0300

    Switch to using `graphql-tools` rather than `graphql`(js).

--- functions/query/index.js
+++ functions/query/index.js
@@ -1,67 +1,56 @@
 const { resolve: urlResolve } = require("url");
 const rp = require("request-promise-native");
-
-const {
-  GraphQLBoolean,
-  GraphQLSchema,
-  GraphQLObjectType,
-  GraphQLString,
-  GraphQLNonNull,
-  GraphQLDirective,
-} = require('graphql')
-
+const { makeExecutableSchema } = require("graphql-tools");
 const { executeGqlLambda } = require("../helpers.js");
 
-const schema = new GraphQLSchema({
-  query: new GraphQLObjectType({
-    name: 'RootQueryType',
-    fields: {
-      greeting: {
-        args: {
-          firstName: {
-            name: 'firstName',
-            type: new GraphQLNonNull(GraphQLString),
-          },
-        },
-        type: GraphQLString,
-        resolve: (parent, args, haveNotLookedUpWhatThisIsTODO, { cacheControl }) => {
-          cacheControl.setCacheHint({maxAge: 60});
-          return `Hello, ${args.firstName}.`;
-        },
-      },
+const typeDefs = `
+type Query {
+  greeting(firstName: String!): String
+  engineHealth: Boolean
+}
 
-      // This query simply shows that the Lambda is aware of its Engine leader,
-      // and could, in theory, communicate with it.
-      engineHealth: {
-        type: GraphQLBoolean,
-        resolve: (parent, args, haveNotLookedUpWhatThisIsTODO, { cacheControl }) => {
-          if (!process.env.ENGINE_PROXY_URL ||
-            // Avoid a serverless test bug where the dynamic env variable is
-            // passed as "[object Object]" when it tries to emulate it.
-            !process.env.ENGINE_PROXY_URL.startsWith("http")) {
-            return false;
-          }
+schema {
+  query: Query
+}
+`;
 
-          return rp({
-              uri: urlResolve(process.env.ENGINE_PROXY_URL,
-                "/.well-known/apollo/engine-health"),
-              resolveWithFullResponse: true,
-            })
-
-            // The Engine Proxy returns status code 200 when all is well.
-            .then((response) => response.statusCode === 200)
-
-            // If an error occurs, we swallow it for now and just return false.
-            .catch(err => {
-              console.error(
-                "Couldn't verify Engine Health due to an error:", err);
-              return false
-            });
-        },
+const resolvers = {
+  Query: {
+    greeting(parent, args, context, { cacheControl }) {
+      cacheControl.setCacheHint({ maxAge: 60 });
+      return `Hello, ${args.firstName}.`;
+    },
+    engineHealth() {
+      if (!process.env.ENGINE_PROXY_URL ||
+        // Avoid a serverless test bug where the dynamic env variable is
+        // passed as "[object Object]" when it tries to emulate it.
+        !process.env.ENGINE_PROXY_URL.startsWith("http")) {
+        return false;
       }
+
+      return rp({
+          uri: urlResolve(process.env.ENGINE_PROXY_URL,
+            "/.well-known/apollo/engine-health"),
+          resolveWithFullResponse: true,
+        })
+
+        // The Engine Proxy returns status code 200 when all is well.
+        .then((response) => response.statusCode === 200)
+
+        // If an error occurs, we swallow it for now and just return false.
+        .catch(err => {
+          console.error(
+            "Couldn't verify Engine Health due to an error:", err);
+          return false
+        });
     }
-  }),
-})
+  },
+};
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 module.exports.default =
   executeGqlLambda((event, context) => {
